83dde852cc12e1f7c2b65039a4506fdbafa2ffd3
David Thaler <davidthaler@gmail.com>
2020-01-06 22:35:29 -0800

    nodes.html / circles.js is adding and removing nodes on click; this includes a check for overlap on node addition.

--- circles.js
+++ circles.js
@@ -1,19 +1,68 @@
 /*
 First step towards GraphMaker on canvas: adding nodes/circles.
 */
-window.onload = function(){
-    const RADIUS = 10
+window.onload = main
 
-    function drawCircle(e){
-        ctx.moveTo(e.x + RADIUS, e.y)
-        ctx.arc(e.x, e.y, RADIUS, 0, 2 * Math.PI)
-        ctx.fill()
-        ctx.stroke()
+const RADIUS = 10
+let canvas, ctx, btnState
+let nodes = []
+
+let stateColor = {
+    'normal'   : {'fill': 'lightgray', 'stroke': 'darkgray'},
+    'selected' : {'fill': 'whitesmoke', 'stroke': 'gray'}
+}
+
+function drawCircle(c){
+    ctx.beginPath()
+    ctx.moveTo(c.x + RADIUS, c.y)
+    ctx.arc(c.x, c.y, RADIUS, 0, 2 * Math.PI)
+    ctx.fill()
+    ctx.stroke()
+}
+
+function clearCanvas(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+function drawNodes(){
+    clearCanvas()
+    nodes.forEach(drawCircle)
+}
+
+//Returns index in nodes of first node within distance d of point x, y
+function withinDist(x, y, d){
+    return nodes.findIndex(n => ((n.x - x)**2 + (n.y - y)**2) <= d**2)
+}
+
+function removeNode(e){
+    if(btnState == 'removeNode'){
+        let nIdx = withinDist(e.x, e.y, RADIUS)
+        if(nIdx != -1){
+            console.log(`found node: ${nIdx}`)
+            nodes.splice(nIdx, 1)
+            drawNodes()
+        }   
     }
+}
 
-    let canvas = document.querySelector('canvas')
-    let ctx = canvas.getContext('2d')
+function addNode(e){
+    if(btnState == 'addNode'){
+        if(withinDist(e.x, e.y, 2 * RADIUS) == -1){
+            nodes.push({x:e.x, y:e.y})
+            drawNodes()
+        }
+    }
+}
+
+function main(){
+    canvas = document.querySelector('canvas')
+    ctx = canvas.getContext('2d')
     ctx.strokeStyle = 'gray'
     ctx.fillStyle = 'lightgray'
-    canvas.addEventListener('click', drawCircle)
+    canvas.addEventListener('click', removeNode)
+    canvas.addEventListener('click', addNode)
+    document.querySelectorAll('input').forEach(b => 
+        b.addEventListener('change', function(){
+                btnState=this.value
+            }))
 }

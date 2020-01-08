db4fec6377df6a48a7099e4600c9426297dcb9c8
David Thaler <davidthaler@gmail.com>
2020-01-07 23:32:45 -0800

    Added color and line width selection to canvas.html/js.

--- canvas.js
+++ canvas.js
@@ -25,10 +25,20 @@ function end(e){
     canvas.removeEventListener('mousemove', draw)
 }
 
-window.onload = function (){
+window.addEventListener('load', function (){
     canvas = document.querySelector('canvas')
     context = canvas.getContext('2d')
     canvas.addEventListener('mousedown', start)
     canvas.addEventListener('mouseup', end)
     canvas.addEventListener('mouseleave', end)
-}
+    document.getElementById('picker')
+            .addEventListener('change', function(){
+                context.strokeStyle = this.value
+            })
+    let wd = document.getElementById('widthDisplay')
+    document.getElementById('widthSelector')
+            .addEventListener('change', function(){
+                context.lineWidth = this.value ** 2
+                wd.textContent = this.value
+            })
+})

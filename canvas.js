e310013cd6db62f6f4171379d5e6330577e81b5c
David Thaler <davidthaler@gmail.com>
2020-01-12 00:08:52 -0800

    Another attempt at fixing the mouse/touch bug. This one is based on getting the start/draw/end functions to work with touch events.

--- canvas.js
+++ canvas.js
@@ -3,26 +3,37 @@ About the simplest sketching program I can think of.
 */
 let canvas, context;
 
-function shiftXY(x, y){
+function getXY(e){
+    let x, y
+    if(e instanceof TouchEvent){
+        [x, y] = e.changedTouches[0]
+    }else{
+        [x, y] = [e.clientX, e.clientY]
+    }
     return [x - canvas.offsetLeft, y - canvas.offsetTop + window.scrollY]
 }
 
 function start(e){
+    e.preventDefault()
     context.beginPath()
-    let [x, y] = shiftXY(e.x, e.y)
+    let [x, y] = getXY(e)
     context.moveTo(x, y)
     canvas.addEventListener('mousemove', draw)
+    canvas.addEventListener('touchmove', draw)
 }
 
 function draw(e){
-    let [x, y] = shiftXY(e.x, e.y)
+    e.preventDefault()
+    let [x, y] = getXY(e)
     context.lineTo(x, y)
     context.stroke()
 }
 
 function end(e){
+    e.preventDefault()
     context.closePath()
     canvas.removeEventListener('mousemove', draw)
+    canvas.removeEventListener('touchmove', draw)
 }
 
 window.addEventListener('load', function (){
@@ -31,6 +42,9 @@ window.addEventListener('load', function (){
     canvas.addEventListener('mousedown', start)
     canvas.addEventListener('mouseup', end)
     canvas.addEventListener('mouseleave', end)
+    canvas.addEventListener('touchstart', start)
+    canvas.addEventListener('touchend', end)
+    canvas.addEventListener('touchcancel', end)
     document.getElementById('picker')
             .addEventListener('change', function(){
                 context.strokeStyle = this.value
